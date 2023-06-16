5a22f4f9d080d59f62a50730bf40ecf58c9b31f9
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-06-16 17:25:24 +0530

    iteration

--- Weak1/practise/js_practice.js
+++ Weak1/practise/js_practice.js
@@ -2,3 +2,13 @@ var user = [
     {name: "Himanshu", age: "23"},
     {name: "Jasaiwal", age: 19}
 ];
+
+for (var i = 0; i < users.length; i++) {
+    const toLog = users[i].name + " s age is" + user[i].age;
+    console.log(toLog)
+}
+
+for (var i = 0; i < users.lenght; i++) {
+    const toLog = "age of " + users[i].name + "is" + users[i].age;
+    console.log(toLog)
+}
