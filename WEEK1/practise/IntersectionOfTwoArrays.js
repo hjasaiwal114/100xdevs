dd2fc855c6f3a62de86ea1f07e38dd6644f67c13
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-06-21 13:53:48 +0530

    solved

--- WEEK1/practise/IntersectionOfTwoArrays.js
+++ WEEK1/practise/IntersectionOfTwoArrays.js
@@ -3,3 +3,17 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+
+var intersection = function(nums1, nums2) {
+    let uniqueNums1 = new Set(nums1);
+    let uniqueNums2 = new Set(nums2);
+    let newList = [];
+
+    uniqueNums2.forEach(value => {
+        if (uniqueNums1.has(value)) {
+            newList.push(value);
+        }
+    });
+
+    return newList;
+}
