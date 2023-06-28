79e0c5eacc11759887a864aa8ccef496da38ed3c
Himanshu jasaiwal <hjasaiwal114@gmail.com>
2023-06-28 23:37:43 +0530

    let see where it gone

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+function calculateSum(counter) {
+    var sum = 0;
+    for (var i = 0; i<counter; i++) {
+        sum = sum + i;
+    }
+    return sum;
+
+}
+
+function handleFirstRequest(req, res) {
+    var calculateSum = calculateSum(100);
+    console.log(calculateSum)
+    var answer = "this sum is " + calculateSum;
+    res.send(answer); 
+}
+
+app.get('/handleSum', handleFirstRequest)
+
+function started() {
+    console.log(` Example app listening on port ${port}`)
+}
+
+app.listen(port, started)
+
+
+
+// node pakage manager
