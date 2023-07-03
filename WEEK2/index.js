29fbe86bfc8051a01ef799b0cbb1f1cad90e60dd
himanshu jasaiwal <68457116+hjasaiwal114@users.noreply.github.com>
2023-07-03 22:56:10 +0000

    boiler plat of express

--- WEEK2/index.js
+++ WEEK2/index.js
@@ -1,23 +1,9 @@
-const fs = require("fs");
 const express = require('express')
 const app = express()
-const port = 3000;
+const port = 3000
 
-function calculateSum(counter) {
-    let total = 0;
-    for (let i = 0 ; i < counter ; i++) {
-        total = total + i;
-    } 
-    return total;
-}
-
-function handleFirstRequest(req, res){
-    let calcul
-    res.send("hello t the 100x devs")
-}
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) =>{
+  res.send('Hello World')
 })
 
 app.listen(port, () => {
