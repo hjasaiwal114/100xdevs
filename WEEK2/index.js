be03aa5a7da0365e10328c2f63613fdd2a6761dc
himanshu jasaiwal <68457116+hjasaiwal114@users.noreply.github.com>
2023-07-04 11:35:29 +0000

    adding querry

--- WEEK2/index.js
+++ WEEK2/index.js
@@ -2,3 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+function handleSum(req, res) {
+  var finalSum = 0;
+  var upTo = req.query.counter;
+  for(var i = 0; i<upTo; i++){
+    finalSum = finalSum + i;
+  }
+  var ans = `Sum is ${finalSum}`
+  res.send(ans);
+}
+
+app.get('/handelSum', handleSum)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, ()  => {
+  console.log(`Example app listening on port ${port}`)
+})
