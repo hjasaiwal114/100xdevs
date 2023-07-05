65d758bedb77429654bb7ea89eff8c6ef7a59194
himanshu jasaiwal <68457116+hjasaiwal114@users.noreply.github.com>
2023-07-05 12:12:59 +0000

    le me move into my system vro

--- WEEK2/index.js
+++ WEEK2/index.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express  = require('express');
+const app = express();
+const port = 3000;
 
-function handleSum(req, res) {
-  var finalSum = 0;
-  var upTo = req.query.counter;
-  for(var i = 0; i<upTo; i++){
-    finalSum = finalSum + i;
-  }
-  var ans = `Sum is ${finalSum}`
-  res.send(ans);
+function handelSum(req, res) {
+    var finalSum = 0;
+    var upTo = res.quey.counter;
+    for (var i=0; i < upTo; i++) {
+        finalSum = finalSum + i;
+    }
+    var ans = `Sum is ${finalSum}`
+    res.send(ans);
 }
 
-app.get('/handelSum', handleSum)
+app.get('/handleSum', handleSum)
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/',(req, res) => {
+    res.send('Hello World!')
 })
 
-app.listen(port, ()  => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
